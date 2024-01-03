d0143cbacd4b066a7ce38385df76cefb0c1622fd
antonnprince <antonprince95@gmail.com>
2024-01-03 22:39:24 +0530

    MASM program to print hello

---
+++ hello.asm
@@ -0,0 +1,22 @@
+DATA SEGMENT
+         MSG DB,"HELLO WORLD","$"
+DATA ENDS
+
+CODE SEGMENT
+          ASSUME CS: CODE,DS:DATA
+        
+    START:
+          MOV    AX,DATA
+          MOV    DS,AX
+        
+          LEA    DX,MSG
+          MOV    AH,06H
+          INT    21H
+        
+    STOP: 
+          MOV    AX,4C00H
+          INT    21H
+
+    
+CODE ENDS
+END START
