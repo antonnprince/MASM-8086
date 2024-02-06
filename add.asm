e78bcd7bf7dbe10b438f5813a9ed1113fda5f2da
antonnprince <antonprince95@gmail.com>
2024-02-06 17:42:21 +0530

    addition fixed

--- add.asm
+++ add.asm
@@ -1,118 +1,93 @@
 DATA SEGMENT
-
 DATA ENDS
 
 CODE SEGMENT
-                 ASSUME CS:CODE,DS:DATA
-        ;ASSUMING I HAVE TO PRINT CONTENT IN AL
+              ASSUME CS:CODE,DS:DATA
+
+
 PRINT PROC
-		
-                 MOV    DL,AL
-                 MOV    AH,02H
-                 INT    21H
-                 RET
+              MOV    DL,AL
+              MOV    AH,02H
+              INT    21H
+              RET
 PRINT ENDP
-	
-        ;STORE ALL 2 NUMBERS
+
 INPUT PROC
-		
-                 MOV    AL,01H
-                 INT    21H
-                 MOV    BL,AL
-		
-                 MOV    AL,01H
-                 INT    21H
-                 MOV    BH,AL
-		
-                 MOV    AL,01H
-                 INT    21H
-                 MOV    CL,AL
-		
-                 MOV    AL,01H
-                 INT    21H
-                 MOV    CH,AL
-		
-		
-                 SUB    BL,48
-                 SUB    BH,48
-                 SUB    CL,48
-                 SUB    CH,48
-		
-        ;BL*10+BH
-		
-                 MOV    AH,0
-                 MOV    AL,10
-                 MUL    BL
-                 MOV    BL,AL
-                 ADD    BL,BH
-		
 
-        ;CL*10+CH
-		
-                 MOV    AH,0
-                 MOV    AL,10
-                 MUL    CL
-                 MOV    CL,AL
-                 ADD    CL,CH
-		
-        ;NOW WE HAVE NUMBERS IN BL AND CL
-                 RET
-		
+              MOV    AH,01H
+              INT    21H
+              MOV    BL,AL
+
+              MOV    AH,01H
+              INT    21H
+              MOV    BH,AL
+
+              MOV    AH,01H
+              INT    21H
+              MOV    CL,AL
+
+              MOV    AH,01H
+              INT    21H
+              MOV    CH,AL
+
+              SUB    BL,48
+              SUB    BH,48
+              SUB    CL,48
+              SUB    CH,48
+
+              MOV    AL,10
+              MUL    BL
+              MOV    BL,AL
+              ADD    BL,BH
+
+              MOV    AL,10
+              MUL    CL
+              MOV    CL,AL
+              ADD    CL,AH
+              RET
 INPUT ENDP
-	
-	
-        ;SO WE NEED TO GET THE DIGITS OF BL
-	
-DISPLAYY PROC
-        ;THE NUMBER IS IN BL
-		
-                 MOV    BH,9
-                 CMP    BH,BL
-                 JC     TWO
-		
-        ONE:     MOV    AL,BL
-                 ADD    AL,48
-                 CALL   PRINT
-                 JMP    ENDD
-		
-        TWO:     MOV    BH,10
-                 MOV    AL,BL
-                 SUB    AH,AH
-                 DIV    BH
-		
-                 MOV    BL,AL
-                 MOV    BH,AH
-		
-                 PUSH   BX
-                 CALL   DISPLAY
-                 POP    BX
 
-                 MOV    AL,BH
-                 ADD    AL,48
-                 CALL   PRINT
-		
-		
-		
-        ENDD:    
-                 RET
-DISPLAYY ENDP
-	
-        START:   
-                 MOV    AX,DATA
-                 MOV    DS,AX
-		
-        ;MAIN PROGRAM
-	
-                 CALL   INPUT
-	
-        ;ADDING TWO NUMBERS AND STORING IN BL
-                 ADD    BL,CL
-	
-	
-                 CALL   DISPLAY
-	
-        STOP:    
-                 MOV    AX,4C00H
-                 INT    21H
+SHOW PROC
+              MOV    BH,9
+              CMP    BH,BL
+              JC     TWO
+
+        ONE:  MOV    AL,BL
+              ADD    AL,48
+              CALL   PRINT
+              JMP    ENDD
+
+        TWO:  
+              mov    bh,10
+              mov    al,bl
+              sub    ah,ah
+              div    bh
+
+              mov    bl,al                  ;q
+              mov    bh,ah                  ;r
+
+              add    al,48
+              call   SHOW
+              mov    bh,al
+              add    al,48
+              call   SHOW
+
+        ENDD: 
+              RET
+
+SHOW ENDP
+        START:
+              MOV    AX,DATA
+              MOV    DS,AX
+
+              CALL   INPUT
+
+              ADD    BL,CL
+
+              CALL   SHOW
+
+        STOP: MOV    AX,4C00H
+              INT    21H
 CODE ENDS
 END START
+      
--- test.asm
+++ test.asm
@@ -1,21 +1,93 @@
 DATA SEGMENT
-           MSG DB,"HELLO","$"
 DATA ENDS
 
 CODE SEGMENT
-            ASSUME CS:CODE, DS:DATA
+            ASSUME CS:CODE,DS:DATA
 
+
+PRINT PROC
+            MOV    DL,AL
+            MOV    AH,02H
+            INT    21H
+            RET
+PRINT ENDP
+
+INPUT PROC
+
+            MOV    AH,01H
+            INT    21H
+            MOV    BL,AL
+
+            MOV    AH,01H
+            INT    21H
+            MOV    BH,AL
+
+            MOV    AH,01H
+            INT    21H
+            MOV    CL,AL
+
+            MOV    AH,01H
+            INT    21H
+            MOV    CH,AL
+
+            SUB    BL,48
+            SUB    BH,48
+            SUB    CL,48
+            SUB    CH,48
+
+            MOV    AL,10
+            MUL    BL
+            MOV    BL,AL
+            ADD    BL,BH
+
+            MOV    AL,10
+            MUL    CL
+            MOV    CL,AL
+            ADD    CL,AH
+            RET
+INPUT ENDP
+
+SHOW PROC
+            MOV    BH,9
+            CMP    BH,BL
+            JC     TWO
+
+      ONE:  MOV    AL,BL
+            ADD    AL,48
+            CALL   PRINT
+            JMP    ENDD
+
+      TWO:  
+            mov    bh,10
+            mov    al,bl
+            sub    ah,ah
+            div    bh
+
+            mov    bl,al                ;q
+            mov    bh,ah                ;r
+
+            add    al,48
+            call   SHOW
+            mov    bh,al
+            add    al,48
+            call   SHOW
+
+      ENDD: 
+            RET
+
+SHOW ENDP
       START:
             MOV    AX,DATA
             MOV    DS,AX
 
-            LEA    DX,MSG
-            MOV    AH,09H
-            INT    21H
+            CALL   INPUT
 
-      STOP: 
-            MOV    AX,4C00H
-            INT    21H
+            ADD    BL,CL
 
+            CALL   SHOW
+
+      STOP: MOV    AX,4C00H
+            INT    21H
 CODE ENDS
 END START
+      
